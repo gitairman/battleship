ef109bdcec1d45e35cada8ce600862bed6d7874d
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-03-23 00:53:34 -0700

    implemented drag and drop v1

--- app.js
+++ app.js
@@ -1,27 +1,93 @@
-const gameBoard = document.querySelector('#gameBoard')
+const gameBoard = document.querySelector('#gameBoard');
+let draggingShip;
 
 const createBoard = (length, width) => {
-  const area = length * width
+  const area = length * width;
   let row = 0,
-    col = 0
+    col = 0;
 
-  gameBoard.style.width = `${width * 50}px`
-  gameBoard.style.height = `${length * 50}px`
+  gameBoard.style.width = `${width * 50}px`;
+  gameBoard.style.height = `${length * 50}px`;
   for (let i = 0; i < area; i++) {
-    console.log(i / length)
-    row = Math.floor(i / length) + 65
-    col = (i % width) + 1
-    gameBoard.insertAdjacentHTML(
-      'beforeend',
-      `<div
-        style="width:${50}px; height:${50}px;"
-        class="tile"
-        id=tile-${i}
-        data-coord=${String.fromCharCode(row) + col}>
-        ${String.fromCharCode(row) + col}
-      </div>`
-    )
-  }
-}
+    // console.log(i / length);
+    row = Math.floor(i / length) + 65;
+    col = (i % width) + 1;
 
-createBoard(10, 10)
+    const tile = document.createElement('div');
+    const coord = String.fromCharCode(row) + col;
+    tile.style.width = tile.style.height = '48px';
+    tile.className = 'tile';
+    tile.id = `tile-${i}`;
+    tile.dataset.coord = coord;
+    tile.innerText = coord;
+    // console.log(tile);
+
+    tile.addEventListener('click', () => {
+      console.log(`You just clicked ${coord}`);
+    });
+    tile.addEventListener('dragenter', (e) => {
+      e.preventDefault();
+
+      e.target.style.backgroundColor = 'blue';
+
+      let count = draggingShip.dataset.size;
+
+      let nextSibling = e.target.nextSibling;
+      while (count > 1) {
+        nextSibling.style.backgroundColor = 'blue';
+        nextSibling = nextSibling.nextSibling;
+        count--;
+      }
+    });
+    tile.addEventListener('dragleave', (e) => {
+      e.preventDefault();
+      e.target.style.backgroundColor = 'transparent';
+    });
+    tile.addEventListener('dragover', (e) => {
+      e.preventDefault();
+      e.dataTransfer.dropEffect = 'move';
+    });
+    tile.addEventListener('drop', (e) => {
+      e.preventDefault();
+      console.log(e.dataTransfer);
+
+      const data = e.dataTransfer.getData('text');
+      console.log(data);
+
+      const ship = document.getElementById(data);
+      console.log(ship);
+
+      e.target.appendChild(ship);
+      ship.style.position = 'absolute';
+    });
+
+    gameBoard.insertAdjacentElement('beforeend', tile);
+  }
+};
+
+const dragStart = (e) => {
+  // console.log(e);
+  e.dataTransfer.setData('text', e.target.id);
+  draggingShip = e.target;
+
+  e.dataTransfer.effectAllowed = 'move';
+};
+
+const handleShipClick = (e) => {
+  const orientation = e.target.style;
+  const vertical = 'rotate(90deg)';
+  const horizontal = 'rotate(0deg)';
+  if (orientation.transform === vertical)
+    return (orientation.transform = horizontal);
+  orientation.transform = vertical;
+};
+
+createBoard(10, 10);
+
+const ships = document.querySelectorAll('.ship');
+console.log(ships);
+
+ships.forEach((ship) => {
+  ship.addEventListener('dragstart', dragStart);
+  ship.addEventListener('click', handleShipClick);
+});
